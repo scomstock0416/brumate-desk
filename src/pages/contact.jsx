e381686bca47a973fdd89ded965f0721d207b10f
Hector Alcazar <halcaza@US-C02DKA1XMD6M>
2021-03-03 16:40:43 -0500

    fix: Contact form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -325,13 +325,13 @@ function IndexPage() {
         errors.name = 'Required'
       }
 
-      if (!values.email) {
-        errors.email = 'Required'
-      } else if (
-        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-      ) {
-        errors.email = 'Invalid email address'
-      }
+      // if (!values.email) {
+      //   errors.email = 'Required'
+      // } else if (
+      //   !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+      // ) {
+      //   errors.email = 'Invalid email address'
+      // }
     }
 
     if (values.picked === '') {
